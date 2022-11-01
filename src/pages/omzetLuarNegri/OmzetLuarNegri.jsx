84c046a83f69acb52464bb378b5aea93a1872004
Hermansyah Putra <hermansyahputra0418@gmail.com>
2022-11-01 23:46:12 +0700

    Ngerjain Omzet luar - belum selesai

---
+++ src/pages/omzetLuarNegri/OmzetLuarNegri.jsx
@@ -0,0 +1,112 @@
+import React, {useState} from 'react'
+import { Container, Row, Button } from "react-bootstrap";
+import NavbarAdmin from "../../components/navbar/NavbarAdmin";
+import Sidebar from "../../components/sidebar/Sidebar";
+
+import {AiOutlineRight} from 'react-icons/ai'
+import {BsChevronExpand} from 'react-icons/bs'
+
+const OmzetLuarNegri = () => {
+
+    const [animated, setAnimated] = useState(false);
+
+  return (
+    <div
+    style={{
+      backgroundColor: "#D9D9D9",
+      minHeight: "100vh",
+    }}
+  >
+    <NavbarAdmin animated={animated} />
+    <Container>
+      <Row className=''>
+        <div style={{background:"salmon"}}>
+          <Sidebar animated={animated} setAnimated={setAnimated} />             
+        </div>
+        <div className="ps-3 " style={{
+          background:"",
+          width: animated ? "85%" : "77%",
+          position:"absolute",
+          transition:"0.5s",
+          left: animated ? "200px" : "300px",
+          top:"125px"
+        }}>
+        <div
+            style={{
+              display:"flex",
+              justifyContent:"space-between",
+              alignItems:"center"
+            }}
+          >
+            <div className="p-2">
+              <h1 className="textHead">Ringkasan Omzet Luar Negeri</h1>
+              <p className="textP">Periode 1 Oktober 2022 - 31 Oktober 2022</p>
+            </div>
+
+              <div style={{
+                display:"flex",
+                alignItems: "center",
+                justifyContent: "flex-end"
+              }} >
+                <div
+                    className=" me-4"
+                >
+                    <h4 style={{
+                        textAlign: "center",
+                        alignItems:"center",
+                    }} className="textChoose shadowElement">
+                        Filter Transaksi
+                        <AiOutlineRight style={{
+                            marginLeft: "5px",
+                            alignItems: "center"
+                        }} />
+                    </h4>
+                </div>
+
+                <div
+                    className="me-4"
+                >
+                    <h4 style={{
+                        textAlign: "center",
+                    }} className=" shadowElement textChoose textChooseColor" >
+                        Pilih Cabang
+                        <BsChevronExpand style={{
+                            marginLeft: "5px",
+                            alignItems: "center"
+                        }} />
+                    </h4>
+                </div>
+                
+                <div
+                    className="me-4"
+                >
+                    <h4 style={{
+                        
+                    }} className=" shadowElement textChoose textChooseColor">
+                        Pilih Bulan
+                        <BsChevronExpand style={{
+                            marginLeft: "5px",
+                            alignItems: "center"
+                        }} />
+                    </h4>
+                </div>
+
+                <Button variant='danger'>Reset Filter</Button>
+
+              </div>
+            
+        </div>
+
+          {/* Components Here */}
+
+
+          {/* Components Here */}
+
+        </div>
+      </Row>
+    </Container>
+  </div>
+  )
+}
+
+export default OmzetLuarNegri
