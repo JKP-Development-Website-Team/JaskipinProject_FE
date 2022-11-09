8e4951cc52787136a6fa7ae1009caf0462f6bc49
Umam1210 <khairulumam950@gmail.com>
2022-11-09 19:13:09 +0700

    penambahan popup di booking page

--- src/components/bookingTable/BookingTable.jsx
+++ src/components/bookingTable/BookingTable.jsx
@@ -1,49 +1,46 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
 import {IoEllipse} from 'react-icons/io5'
+import PopupBooking from './PopupBooking'
 
 
 const BookingTable = () => {
     const datas = [
         {
             nama:'Jhon Doe',
-            nomor:'123123123',
+            no_booking :'00000000',
+            no_hp:'0858000000',
+            isi_paket: 'makanan',
             tanggal:'22 maret 2022',
             berat:'2 Kg',
             status:'Sudah dipickup',
             tujuan:'Aceh',
+            jns_paket:'paket',
+            asal :'cirebon',
+            kurir: 'Si Cepat',
+            estimasi: '2-3 hari',
+            harga: '15000',
+            w_penjemputan: 'sumber',
+            alamat :'jl. lorem',
+            volume : '22 X 10 X 10'
         },
         {
-            nama:'Ujang Suryana',
-            nomor:'1231230000',
-            tanggal:'13 april 2022',
-            berat:'1 Kg',
-            status:'Belum dipickup',
-            tujuan:'Garut',
-        },
-        {
-            nama:'Jody Cmieww',
-            nomor:'123123333',
-            tanggal:'10 oktober 2022',
-            berat:'23 Kg',
-            status:'Sudah dipickup',
-            tujuan:'Maluku',
-        },
-        {
-            nama:'Abel',
-            nomor:'123123123',
-            tanggal:'01 november 2022',
-            berat:'4 Kg',
-            status:'Belum dipickup',
-            tujuan:'Yogyakarta',
-        },
-        {
-            nama:'Jhon',
-            nomor:'123123123',
-            tanggal:'20 maret 2022',
+            nama:'Jhon wick',
+            no_booking :'00000000',
+            no_hp:'0858000000',
+            isi_paket: 'makanan',
+            tanggal:'22 maret 2022',
             berat:'2 Kg',
             status:'Sudah dipickup',
             tujuan:'Aceh',
+            jns_paket:'paket',
+            asal :'cirebon',
+            kurir: 'Si Cepat',
+            estimasi: '2-3 hari',
+            harga: '15000',
+            w_penjemputan: 'sumber',
+            alamat :'jl. lorem',
+            volume : '22 X 10 X 10'
         },
     ]
 
@@ -74,13 +71,13 @@ const BookingTable = () => {
                     {datas.map((data, index) => (
                     <tr>
                     <td>{data.nama}</td>
-                    <td>{data.nomor}</td>
+                    <td>{data.no_hp}</td>
                     <td>{data.tanggal}</td>
                     <td>{data.berat}</td>
                     <td>{data.status}</td>
                     <td>{data.tujuan}</td>
                     <td className='d-flex gap-3'>
-                        <Button style={{
+                        {/* <Button style={{
                             background:"#9D9C9C",
                             border:"none",
                             padding:"5px 1em 5px 8px",
@@ -89,8 +86,26 @@ const BookingTable = () => {
                         }}>
                             <span style={{
                                 paddingRight:"5px",
-                            }}><IoEllipse /></span> Detail
-                        </Button>
+                            }}><IoEllipse /></span> 
+                        </Button> */}
+                            <PopupBooking 
+                            no_booking = {data.no_booking}
+                            nama = {data.nama}
+                            no_hp = {data.no_hp}
+                            alamat = {data.alamat}
+                            tanggal ={data.tanggal}
+                            berat = {data.berat}
+                            isi_paket ={data.isi_paket}
+                            volume ={ data.volume}
+                            jns_paket ={data.jns_paket}
+                            asal={data.asal}
+                            tujuan={data.tujuan}
+                            kurir={data.kurir}
+                            estimasi={data.estimasi}
+                            harga ={data.harga}
+                            w_penjemputan={data.w_penjemputan}
+                            
+                            />
                         <Button style={{
                             background:"#9D9C9C",
                             border:"none",
